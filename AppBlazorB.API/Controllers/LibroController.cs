c0af8655f1a3add669b29dbfc2acbd000ed6b788
brezzieC <sindelmayta98@gmail.com>
2025-09-29 10:46:20 -0400

    BD

---
+++ AppBlazorB.API/Controllers/LibroController.cs
@@ -0,0 +1,71 @@
+﻿using AppBlazor.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppBlazorB.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LibroController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult listarLibros()
+        {
+            try
+            {
+                return Ok();
+            }catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+        [HttpGet("{idlibro}")]
+        public IActionResult recuperarLibro(int idlibro)
+        {
+            try
+            {
+               return Ok();
+            }
+            catch (Exception ex)
+            {
+                    return StatusCode(500, ex.Message);
+            }
+        }
+            [HttpDelete("{idlibro}")]
+            public IActionResult eliminarLibro(int idlibro)
+            {
+                try
+                {
+                    return Ok();
+                }
+                catch (Exception ex)
+                {
+                    return StatusCode(500, ex.Message);
+                }
+            }
+        [HttpPost]
+        public IActionResult guardarLibro([FromBody]LibroFormCLS libroFormCLS)   
+        {
+            try
+            {
+                return Ok();
+            }catch(Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+        [HttpGet("recuperarArchivo/{idlibro}")]
+        public IActionResult recuperarArchivoPorId(int idlibro)
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+    }
+}
+
---
+++ AppBlazorB.API/Controllers/TipoLibroController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppBlazorB.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TipoLibroController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult listarTipoLibros()
+        {
+            try
+            {
+                return Ok();
+            }catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+    }
+}
